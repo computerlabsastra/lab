1ec4d3e9320981550a7ef0b754bdff82280765af
computerlabsastra <119721986+computerlabsastra@users.noreply.github.com>
2022-12-05 12:50:49 +0530

    Add files via upload

---
+++ binomialtheoram.c
@@ -0,0 +1,26 @@
+//binomial theoram using c
+#include <stdio.h> <math.h>
+factorial (int z)
+{
+    for (int y=1;y<=z;y++)
+    {
+        
+    }
+}
+comput(int a,b)
+{
+
+}
+main()
+{
+    int n,b=0;
+    printf("Hey , you're here to find binomial expansion using c.\nThe format is (x+y)^n.\nGive the values of n:");
+    scanf("%d",&n);
+    printf("Hence , the expansion is\n");
+    for (int i=0;i<=n;i++)
+    {
+        comput(n,i)=a[b];
+        b++;
+        printf("%d\n",a[b]);
+    }
+}
